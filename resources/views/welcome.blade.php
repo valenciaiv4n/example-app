4ef7ba6cf0e276d62871e041fdab0499a7ff50f0
TiwexCr25 <130935030+TiwexCr25@users.noreply.github.com>
2024-11-07 19:31:51 -0800

    cambie el titulo
    puse cristian

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel cristian</title>
+        <title>Laravel cristian2</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
